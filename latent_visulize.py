f444c344c5d05e862e3e4678ef95a6467e6d76bd
Zi Huang <rayfilm@foxmail.com>
2025-03-06 12:10:47 +0100

    add mask and scale visualization.

--- latent_visulize.py
+++ latent_visulize.py
@@ -11,6 +11,70 @@ import matplotlib.pyplot as plt
 # from sklearn.cluster import DBSCAN
 from sklearn.manifold import TSNE
 import umap
+from mpl_toolkits.mplot3d import Axes3D
+import plotly.express as px
+
+def get_mask(file_path, mask_num):
+    # color, shape, scale, orientation, pos_x, pos_y
+    data = np.load(file_path)
+    latents_values = data["latents_values"][:]
+
+    # shape: square1, ellipse2, heart3
+
+    ## the pivot image under different x position
+    # key = np.array([[1.,3.,0.7,3.22,0.16,0.48]])
+    # pivot_index = 594095
+    # key = np.array([[1.,3.,0.7,3.22,0.0,0.48]])
+    # pivot_index = 593935
+    # key = np.array([[1.,3.,0.7,3.22,0.48,0.48]])
+    # pivot_index = 594415
+    ## pos = np.where(np.all(np.isclose(self.latents_values, key, atol=1e-2), axis=1))[0]
+
+    # color, shape, scale, orientation, pos_x, pos_y
+    # shape: square1, ellipse2, heart3
+    shape_ = latents_values[:, 1]
+    scale = latents_values[:, 2]
+    rotation = latents_values[:, 3]
+    position_x = latents_values[:, 4]
+    position_y = latents_values[:, 5]
+
+    # Mask only change pos_y
+    if mask_num == 0:
+        # Mask only change pos_x
+        mask = (
+                (shape_ == 3.) &
+                (scale == 0.7) &
+                (np.isclose(rotation, 3.22214631, atol=1e-2)) &
+                (np.isclose(position_y, 0.48387097, atol=1e-2))
+        )
+    if mask_num == 1:
+        # mask for orientation
+        mask = (
+                #(shape_ == 3.) &
+                # (scale == 0.7) &
+                # (np.isclose(position_x, 0.16, atol=1e-2)) &
+                (np.isclose(position_y, 0.48387097, atol=1e-2))
+        )
+
+    # mask for size
+    if mask_num == 4: # use only x
+        mask = (
+                # (shape_ == 3.) &
+                # (scale == 0.7) &
+                (np.isclose(rotation, 3.22214631, atol=1e-2)) &
+                # (np.isclose(position_x, 0.16, atol=1e-2)) &
+                (np.isclose(position_y, 0.48387097, atol=1e-2))
+        )
+    if mask_num == 5: # use only y
+        mask = (
+                # (shape_ == 3.) &
+                # (scale == 0.7) &
+                (np.isclose(rotation, 3.22214631, atol=1e-2)) &
+                (np.isclose(position_x, 0.16, atol=1e-2))
+                # (np.isclose(position_y, 0.48387097, atol=1e-2))
+        )
+    result = mask
+    return result
 
 def encode_img(file_path):
     data = np.load(file_path)
@@ -29,31 +93,8 @@ def encode_img(file_path):
     ## pos = np.where(np.all(np.isclose(self.latents_values, key, atol=1e-2), axis=1))[0]
     pivot_index = 594095
     p_img = images[pivot_index]
-    values = latents_values
     # color, shape, scale, orientation, pos_x, pos_y
-    shape_ = values[:, 1]
-    scale = values[:, 2]
-    rotation = values[:, 3]
-    position_x = values[:, 4]
-    position_y = values[:, 5]
 
-    # Mask only change pos_y
-    # shape: square1, ellipse2, heart3
-    # mask = (
-    #         (shape_ == 3.) &
-    #         (scale == 0.7) &
-    #         (rotation == 3.22) &
-    #         (position_x == 0.16)
-    # )
-
-    # Mask only change pos_x
-    mask = (
-            (shape_ == 3.) &
-            (scale == 0.7) &
-            (np.isclose(rotation, 3.22214631, atol=1e-2)) &
-            (np.isclose(position_y, 0.48387097, atol=1e-2))
-    )
-    result = images[mask]
     p_img = torch.tensor(p_img.reshape(1,1, 64, 64), dtype=torch.float32)
     imgs = torch.tensor(images.reshape(-1, 1, 64, 64), dtype=torch.float32)
     # result = torch.tensor(result.reshape(-1,1, 64, 64), dtype=torch.float32)
@@ -135,21 +176,54 @@ if __name__ == "__main__":
     # plt.show()
 
     ## see scale
-    # z_gt1 = z_gt[0:-1:10][:, 2].reshape(-1)  # scale
-    # z_embedded = umap.UMAP(n_neighbors=80).fit_transform(z)
+    mask = get_mask("./data/2d/dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz", 4)
+    z1 = np.load("./outputs/latent_vir.npy")
+    z1 = z1[mask]
+    z_gt1 = z_gt[mask][:, 2].reshape(-1)  # scale
+    z_embedded1 = umap.UMAP(n_neighbors=80).fit_transform(z1)
+
+    mask = get_mask("./data/2d/dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz", 5)
+    z2 = np.load("./outputs/latent_vir.npy")
+    z2 = z2[mask]
+    z_gt2 = z_gt[mask][:, 2].reshape(-1)  # scale
+    z_embedded2 = umap.UMAP(n_neighbors=80).fit_transform(z1)
+
+    fig, ax = plt.subplots(1,2, figsize=(18, 10))
+    sc1 = ax[0].scatter(*z_embedded1.T, s=10, c=z_gt1, alpha=0.5)
+    ax[0].set_title("fix pos_y")
+    sc2 = ax[1].scatter(*z_embedded2.T, s=10, c=z_gt2, alpha=0.5)
+    ax[1].set_title("fix pos_x")
+
+    for a in ax:
+        a.set_xticks([])
+        a.set_yticks([])
+    fig.colorbar(sc1, ax=ax[0])
+    fig.colorbar(sc2, ax=ax[1])
+    plt.show()
+
+    ## see orientation
+    # mask = get_mask("./data/2d/dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz", 1)
+    # z = np.load("./outputs/latent_vir.npy")
+    # z = z[mask]
+    # z_gt1 = z_gt[mask][:, 3].reshape(-1)  # orientation
+    # z_embedded = umap.UMAP(n_neighbors=5,n_components=2).fit_transform(z)
     # fig, ax = plt.subplots(1, figsize=(14, 10))
-    # plt.scatter(*z_embedded.T, s=0.3, c=z_gt1, alpha=0.3)
+    # plt.scatter(*z_embedded.T, s=10, c=z_gt1, alpha=0.3)
     # plt.setp(ax, xticks=[], yticks=[])
     # cbar = plt.colorbar()
     # cbar.set_ticks(z_gt1)
     # plt.show()
 
-    ## see orientation
-    z_gt1 = z_gt[0:-1:10][:, 3].reshape(-1)  # orientation
-    z_embedded = umap.UMAP(n_neighbors=80).fit_transform(z)
-    fig, ax = plt.subplots(1, figsize=(14, 10))
-    plt.scatter(*z_embedded.T, s=0.3, c=z_gt1, alpha=0.3)
-    plt.setp(ax, xticks=[], yticks=[])
-    cbar = plt.colorbar()
-    cbar.set_ticks(z_gt1)
-    plt.show()
+    # # Create interactive 3D scatter plot with custom point size
+    # fig = px.scatter_3d(
+    #     x=z_embedded[:, 0],
+    #     y=z_embedded[:, 1],
+    #     z=z_embedded[:, 2],
+    #     color=z_gt1,
+    #     opacity=0.9
+    # )
+    #
+    # # Adjust marker size
+    # fig.update_traces(marker=dict(size=1))  # Change 3 to any desired size
+    #
+    # fig.show()
